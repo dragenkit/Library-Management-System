528d3addae6334ae71ee6889f35c855cad7a6525
dragenkit <marvinauner47@gmail.com>
2025-02-13 09:04:43 +0100

    started login windo

--- src/main/Login.java
+++ src/main/Login.java
@@ -1,5 +1,27 @@
 package src.main;
 
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Login {
+
+    JFrame loginView;
+    JButton adminButton, librarianButton;
+
+    Login(){
+
+        loginView = new JFrame();
+        loginView.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        loginView.setLayout(new BorderLayout());
+
+        adminButton = new JButton("Admin Login");
+
+        librarianButton = new JButton("Librarian Login");
+
+
+        loginView.add(adminButton, BorderLayout.SOUTH);
+        loginView.add(librarianButton, BorderLayout.CENTER);
+        loginView.setVisible(true);
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,7 @@
 package src.main;
 
 public class Main {
-    public static void Main(String[] args) {
-
+    public static void main(String[] args) {
+       Login login = new Login();
     }
 }
